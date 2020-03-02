bb80a8551143cda362e8fbc01edc1a24c8956cdb
Carlos Torres <carlos.torres@udc.es>
2020-03-02 16:24:29 +0100

    Resources outside package

--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -1,7 +1,15 @@
 package es.udc.redes.webserver;
 
+/**
+ *
+ * @author Carlos Torres (carlos.torres@udc.es)
+ */
 public class WebServer {
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
     }
